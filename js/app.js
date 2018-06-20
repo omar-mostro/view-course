7b8bcdfc3679d9e14603550f49d94c5c94cf5fec
Omar Sánchez Martínez <omar@posibilidades.com.mx>
2018-06-20 17:36:47 -0500

    project 1 complete
    Proyecto de derrotar al monstruo

--- js/app.js
+++ js/app.js
@@ -1,17 +1,90 @@
 new Vue({
-    el: '#exercise',
+    el: '#app',
     data: {
-        show: true,
-        array: ['Max', 'Anna', 'Chris', 'Manu'],
-        myObject: {
-            title: 'Lord of the Rings',
-            author: 'J.R.R. Tolkiens',
-            books: '3'
+        startNewGame: false,
+        humanLife: 100,
+        monsterLife: 100,
+        turns: []
+    },
+    methods: {
+        startGame: function () {
+            this.startNewGame = true;
+            this.humanLife = 100;
+            this.monsterLife = 100;
+            this.turns = []
+
         },
-        testData: {
-            name: 'TESTOBJECT',
-            id: 10,
-            data: [1.67, 1.33, 0.98, 2.21]
+        attack: function () {
+
+            let damage = this.getRandomInt(8, 15);
+            this.turns.unshift({
+                isHuman: true,
+                text: 'Player hits monster for ' + damage
+            });
+            this.monsterLife -= damage;
+
+
+            this.monsterAttacks();
+
+            this.winStatus();
+        },
+        specialAttack: function () {
+            let damage = this.getRandomInt(8, 20);
+            this.turns.unshift({
+                isHuman: true,
+                text: 'Player hits monster attack for ' + damage
+            });
+            this.monsterLife -= damage;
+            this.monsterAttacks();
+            this.winStatus();
+
+        },
+        heal: function () {
+            if (this.humanLife < 90) {
+                let heal = this.getRandomInt(2, 20)
+                this.humanLife += heal ;
+                this.turns.unshift({
+                    isHuman: true,
+                    text: 'Player heals for ' + heal
+                });
+            }
+            this.monsterAttacks();
+
+        },
+        giveUp: function () {
+            this.startNewGame = false;
+            this.humanAttack = false;
+            this.humanLife = 100;
+            this.monsterLife = 100;
+        },
+        monsterAttacks: function () {
+            let damage = this.getRandomInt(1, 15);
+            this.turns.unshift({
+                isHuman: false,
+                text: 'Monster hits Player for ' + damage
+            });
+            this.humanLife -= damage
+        },
+        getRandomInt: function (min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+        winStatus: function () {
+            if (this.humanLife <= 0) {
+                if (confirm('Perdiste, ¿deseas comenzar un nuevo juego?')) {
+                    this.startGame()
+                }
+
+                return false
+            }
+
+            if (this.monsterLife <= 0) {
+                if (confirm('Ganaste, deseas comenzar un nuevo juego?')) {
+                    this.startGame()
+                }
+
+                return false
+            }
         }
     }
 });
+
