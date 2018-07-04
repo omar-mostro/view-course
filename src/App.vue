b46544638f4a469c2fd41de467636c3d828cfc01
Omar Sánchez Martínez <omar@posibilidades.com.mx>
2018-07-04 12:41:45 -0500

    task 1 form third module complete
    esta tarea es sobre los formularios y lo más complicado sobre como crear input personalizados en componentes e interactuar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,70 @@
 <template>
   <div class="container">
-    <osm-header :numberQuotes="quotes.length" :maxQuotes="maxQuotes"></osm-header>
-    <osm-new-quote @quoteAdded="newQuote"></osm-new-quote>
-    <osm-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></osm-quote-grid>
+    <form>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <!-- Exercise 1 -->
+          <!-- Create a Signup Form where you retrieve the following Information -->
+          <!-- Full Name (First Name + Last Name) -->
+          <!-- Mail -->
+          <!-- Password -->
+          <!-- Store Data? Yes/No -->
+          <osm-full-name :email="email" v-model="fullName"></osm-full-name>
+
+          <div class="form-group">
+            <label for="email">Email address:</label>
+            <input v-model="email" type="text" id="email" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="Password">Password:</label>
+            <input required v-model="password" type="text" id="Password" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Store Data?:</label>
+            <div class="radio">
+              <label>
+                <input :selected="store === 'yes'" v-model="store" value="Yes" type="radio">
+                Yes
+              </label>
+            </div>
+            <div class="radio">
+              <label>
+                <input v-model="store" value="No" type="radio">
+                No
+              </label>
+            </div>
+
+            <!-- Exercise 2 -->
+            <!-- Only display the Form if it has NOT been submitted -->
+            <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+            <div class="form-group text-center">
+              <button class="btn btn-success" type="submit" @click.prevent="show = true">Submit</button>
+            </div>
+
+          </div>
+
+
+          <!-- Exercise 3 -->
+          <!-- Edit the Example from above and create a custom "Full Name" Control -->
+          <!-- which still holds the First Name and Last Name Input Field -->
+        </div>
+      </div>
+    </form>
+    <hr>
     <div class="row">
-      <div class="col-12">
-        <div class="alert alert-info text-center">
-          <strong>Click a quote to delete it</strong>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p> Name: {{fullName.name}}</p>
+            <p>LastName: {{fullName.lastName}}</p>
+            <p>Mail: {{email}}</p>
+            <p>Password: {{password}}</p>
+            <p>Store in Database?: {{store}}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -14,33 +72,23 @@
 </template>
 
 <script>
-
-  import Header from './components/Header';
-  import QuoteGrid from './components/QuoteGrid';
-  import NewQuote from './components/NewQuote';
-
+  import FullName from './FullName';
   export default {
     data() {
       return {
-        quotes: ['Just a quote to see something'],
-        maxQuotes: 5,
-      }
-    },
-    methods: {
-      newQuote(quote) {
-        if (this.quotes.length >= this.maxQuotes)
-          return alert('Cant create more than 10 quotes');
+        fullName: {
+          name: 'Omar',
+          lastName: 'Sánchez',
+        },
+        email: 'email',
+        password: '',
+        store: 'yes',
+        show: false
 
-        this.quotes.push(quote);
-      },
-      deleteQuote(index){
-        this.quotes.splice(index, 1);
       }
     },
     components: {
-      osmHeader: Header,
-      osmQuoteGrid: QuoteGrid,
-      osmNewQuote: NewQuote,
+      osmFullName : FullName
     }
   }
 </script>
---
+++ src/FullName.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label >Name:</label>
+      <input :value="value.name" @input="changedName(false, $event)" type="text" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="lastName">Last Name:</label>
+      <input :value="value.lastName" @input="changedName(true, $event)" type="text" id="lastName" class="form-control">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props : ['value'],
+    methods:{
+      changedName(isLastname, event){
+
+        if (isLastname)
+          this.value.lastName = event.target.value;
+
+        else
+          this.value.name = event.target.value;
+
+
+        this.$emit('input', this.value)
+      }
+    },
+
+  }
+</script>
+
+<style scoped>
+  input{
+    background-color: darkslategray;
+    color: #fff;
+  }
+</style>
