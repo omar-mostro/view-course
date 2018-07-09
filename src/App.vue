5266e4b342d6a746771cc21ec7075680777ddc06
Omar Sánchez Martínez <omar@posibilidades.com.mx>
2018-07-09 11:14:45 -0500

    routes with vue

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,59 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Http</h1>
-        <div class="form-group">
-          <label for="">User name</label>
-          <input type="text" class="form-control" v-model="user.userName">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
+                <hr>
+                <router-view name="header-top"></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+                <router-view name="header-bottom"></router-view>
+            </div>
         </div>
-        <div class="form-group">
-          <label for="">Mail</label>
-          <input type="email" class="form-control" v-model="user.email">
-        </div>
-
-        <div class="form-group">
-          <label for="">end URL</label>
-          <input type="email" class="form-control" v-model="node">
-        </div>
-
-        <div class="form-group">
-          <button @click="submit" class="btn btn-primary">Submit</button>
-        </div>
-
-
-        <div class="form-group">
-          <button @click="fetchData" class="btn btn-primary">Get Data</button>
-        </div>
-
-        <ul class="list-group">
-          <li class="list-group-item" v-for="userReturned in users">
-            <p>Nombre : {{userReturned.userName}}</p>
-            <p>Email : {{userReturned.email}}</p>
-          </li>
-        </ul>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        user: {
-          userName: '',
-          email: ''
-        },
-        users: [],
-        resource: {},
-        node: 'data'
-      }
-    },
-    methods: {
-      submit() {
-        /* this.$http.post('data.json', this.user)
-           .then(response => {
-               console.log(response)
-             },
-             error => {
-               console.log(error)
-             });*/
-
-        this.resource.saveAlt({}, this.user)
-      },
-      fetchData() {
-        /* this.$http.get('data.json')
-           .then(response => {
-             return response.json()
-           })
-           .then(data => {
-             let resultArray = [];
-             for (let key in data){
-               resultArray.push(data[key])
-             }
-
-             this.users = resultArray;
-           });*/
-
-        this.resource.getData({node: this.node})
-          .then(response => {
-            return response.json()
-          })
-          .then(data => {
-            let resultArray = [];
-            for (let key in data){
-              resultArray.push(data[key])
-            }
-
-            this.users = resultArray;
-          });
-
-      }
-    },
-    created() {
-      const customActions = {
-        saveAlt: {method: 'POST', url: 'alternative.json'},
-        getData: {method: 'GET'},
-
-      };
-      this.resource = this.$resource('{node}.json', {}, customActions);
+    import Header from './components/Header.vue';
+    export default {
+        components: {
+            appHeader: Header
+        }
     }
-  }
 </script>
 
 <style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,31 @@
-import Vue from 'vue';
-import VueResource from 'vue-resource';
-import App from './App.vue';
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import App from './App.vue'
+import { routes } from './routes';
 
-Vue.use(VueResource);
-Vue.http.options.root = 'https://vuejs-http-7b718.firebaseio.com/';
-Vue.http.interceptors.push((request, next) => {
-//interceting request
-  if (request.method === 'POST') {
-    request.method = 'PUT';
-  }
-  //intercepting responses
-  next(response => {
-    response.json = () => {
-      return {messas: response.body}
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
     }
-  })
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return {x: 0, y: 0};
+  }
 });
+
+router.beforeEach((to, from, next) => {
+  console.log('global beforeEach');
+  next();
+});
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
